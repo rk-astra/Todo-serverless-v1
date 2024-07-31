5f9ede3a687ef811b0249f4d14c147321c4b27d1
rk-astra <rakshitksingh@gmail.com>
2024-07-31 09:01:06 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,3 +3,8 @@ const mongoose = require("mongoose");
 mongoose.connect(DATABASE_URI)
 .then(() => { console.log("connected to database")
 .catch((e) => {console.log("connection to db failed");
+
+const userSchema = mongoose.Schema({
+  username:{},
+  password:{}
+})
